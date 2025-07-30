4b3d14081b09482df7680a90e17eef50bb7a2780
jon-castro <jcastrogarcia@gmail.com>
2025-07-30 03:06:54 -0500

    Added test file

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	var finalSlice []string
+	stripedText := strings.TrimSpace(strings.ToLower(text))
+	finalSlice = strings.Fields(stripedText)
+
+	return finalSlice
 }
